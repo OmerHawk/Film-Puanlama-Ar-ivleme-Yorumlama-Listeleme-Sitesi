62d5d9659ed8ff6cb93b914f443d3d6b2f7a5fba
turanbrak <turanburakxx@gmail.com>
2026-05-03 19:02:37 +0300

    __init__.py dosyası güncellendi ve db bağlantısı eklendi

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'gizli-anahtar'
+    app.config['SECRET_KEY'] = 'gizli-anahtar-buraya'
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///veritabani.db'
+    
+    db.init_app(app)
 
     from .views import views
     app.register_blueprint(views, url_prefix='/')
+
+    with app.app_context():
+        from . import models 
+        db.create_all()      
 
     return app
